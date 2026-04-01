cd59066d73945e9b2d75aa50f8743023a16414ab
originEdu <originpark82@gmail.com>
2026-04-01 10:20:29 +0900

    라이브러리 추가

--- main.cpp
+++ main.cpp
@@ -1,48 +1,57 @@
 ﻿#include<iostream>
 #include"Engine.h"
+#include "SDL.h"
+#include "SDL_main.h"
+
+int SDL_main(int arg, char* argv)
+{
+	return 0;
+}
+
+
 
 void SelectionSort(int* Data, int Size, char Order);
 void BubbleSort(int* Data, int Size, bool (*cmp)(int, int));
 bool Compare(int a, int b, char Order);
 bool Asc(int a, int b);
 bool Desc(int a, int b);
-int main()
-{
-	UEngine::GetInstance();
-	UEngine::Instance->GetWorld()->Load("TestMap.txt");
-	UEngine::Instance->Run();
-	delete UEngine::Instance;
-
-	//Sort + 함수 포인터
-	//int Data[10] = { 9,2,3,4,5,6,7,8,1,10 };
-
-	//SelectionSort(Data, 10, 'A'); // 오름차순
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	std::cout << Data[i] << ", ";
-	//}
-	//std::cout << std::endl;
-	//SelectionSort(Data, 10, 'D'); // 내림차순
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	std::cout << Data[i] << ", ";
-	//}
-	//std::cout << std::endl;
-	////버블정렬
-	//BubbleSort(Data, 10, Asc);
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	std::cout << Data[i] << ", ";
-	//}
-	//std::cout << std::endl;
-	//BubbleSort(Data, 10, Desc);
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	std::cout << Data[i] << ", ";
-	//}
-
-	return 0;
-}
+//int main()
+//{
+//	UEngine::GetInstance();
+//	UEngine::Instance->GetWorld()->Load("TestMap.txt");
+//	UEngine::Instance->Run();
+//	delete UEngine::Instance;
+//
+//	//Sort + 함수 포인터
+//	//int Data[10] = { 9,2,3,4,5,6,7,8,1,10 };
+//
+//	//SelectionSort(Data, 10, 'A'); // 오름차순
+//	//for (int i = 0; i < 10; i++)
+//	//{
+//	//	std::cout << Data[i] << ", ";
+//	//}
+//	//std::cout << std::endl;
+//	//SelectionSort(Data, 10, 'D'); // 내림차순
+//	//for (int i = 0; i < 10; i++)
+//	//{
+//	//	std::cout << Data[i] << ", ";
+//	//}
+//	//std::cout << std::endl;
+//	////버블정렬
+//	//BubbleSort(Data, 10, Asc);
+//	//for (int i = 0; i < 10; i++)
+//	//{
+//	//	std::cout << Data[i] << ", ";
+//	//}
+//	//std::cout << std::endl;
+//	//BubbleSort(Data, 10, Desc);
+//	//for (int i = 0; i < 10; i++)
+//	//{
+//	//	std::cout << Data[i] << ", ";
+//	//}
+//
+//	return 0;
+//}
 
 bool Compare(int a, int b, char Order) {
 	return (Order == 'A') ? (a > b) : (a < b);
