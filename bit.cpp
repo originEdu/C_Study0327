07c7b9a9ec70433c97e58a96ed846c4ac70aa26f
originEdu <originpark82@gmail.com>
2026-04-02 11:50:53 +0900

    비트연산

---
+++ bit.cpp
@@ -0,0 +1,29 @@
+﻿//#include<iostream>
+//int SDL_main(int arg, char* argv[])
+//{
+//
+//	int N = 0;
+//	unsigned long long X = 1;
+//	unsigned long long XOR = 0;
+//
+//	std::cin >> N;
+//	for (int i = 0; i < N; i++)
+//	{
+//		std::cin >> X;
+//
+//		for (int j = 0; j < 63; j++)
+//		{
+//			//if (X <= std::pow(2, j))
+//			unsigned long long NPOT = (1 << j);
+//			if (X <= NPOT)
+//			{
+//				XOR = XOR ^ NPOT;
+//				break;
+//			}
+//		}
+//	}
+//
+//
+//	std::cout << XOR << std::endl;
+//	return 0;
+//}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 ﻿#include<iostream>
 #include"Engine.h"
-#include "World.h"
+
+
 
 int SDL_main(int arg, char* argv[])
 {
-
-	Uint64 CurrentTime = SDL_GetTicks64();
-	
 	/*UEngine::GetInstance();
 	UEngine::Instance->GetWorld()->Load("TestMap.txt");
 	UEngine::Instance->Run();
